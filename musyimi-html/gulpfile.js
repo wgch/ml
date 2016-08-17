660e07135b43720e2056a32be18ccc404b184c3b
GW <gichuwil@gmail.com>
2016-08-17 05:54:57 +0300

    Minor updates

--- musyimi-html/gulpfile.js
+++ musyimi-html/gulpfile.js
@@ -10,7 +10,7 @@ gulp.task('serve', ['sass'], function() {
 	});
 
 	gulp.watch("scss/*.scss", ['sass']);
-	gulp.watch("app/*.html").on('change', browserSync.reload);
+	gulp.watch("app/**/*.html").on('change', browserSync.reload);
 });
 
 // Compile sass into CSS & auto-inject into browsers
